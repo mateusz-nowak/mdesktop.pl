cf42ca532d7d2f345a16f9544b42ff34fa53f468
Mateusz Nowak <matixe@gmail.com>
2012-12-01 15:33:56 +0100

    update

--- src/Acme/MainBundle/Command/MoviesCommand.php
+++ src/Acme/MainBundle/Command/MoviesCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\DomCrawler\Crawler;
 use Acme\MainBundle\Entity\Category;
 use Acme\MainBundle\Entity\Movie;
 use Buzz\Browser;
+use Symfony\Component\Console\Input\InputArgument;
 
 class MoviesCommand extends ContainerAwareCommand
 {
@@ -16,29 +17,36 @@ class MoviesCommand extends ContainerAwareCommand
     {
         $this
             ->setName('main:movies')
-            ->setDescription('Search for movies update');
+            ->setDescription('Search for movies update')
+			->addArgument(
+                'pages',
+                InputArgument::OPTIONAL,
+                'How many pages deph does the task follow?'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+    	$pages = $input->getArgument('pages') ? $input->getArgument('pages') : 1351;
+		
         $browser = new Browser;
         $em = $this->getContainer()->get('doctrine.orm.entity_manager');
 
-        for ($i = 1; $i <= 1351; ++$i) {
-            echo sprintf(">> Loading %d of %d movies...\n", $i*10, 13510);
+        for ($i = 1; $i <= $pages; ++$i) {
+            echo sprintf(">> Loading %d of %d movies...\n", $i*10, $pages*10);
 
             $response = $browser->get(sprintf('http://kinoland.pl/videos?page=%d', $i));
             $crawler = new Crawler;
             $crawler->addHtmlContent((string) $response);
 
             foreach ($crawler->filter('.film-main') as $movie) {
-                preg_match('/Kategorie: (?P<categories>.*)?/', $movie->getElementsByTagName('div')->item(1)->nodeValue, $tmpCategory);
+                preg_match('/Kategorie: (?P<categories>.*)?/', str_replace(PHP_EOL, '', $movie->getElementsByTagName('div')->item(1)->nodeValue), $tmpCategory);
                 preg_match('/(?<title>.*?) \[(?<translation>.*?)\]/', $movie->getElementsByTagName('h2')->item(0)->nodeValue, $tmpData);
                 preg_match('/film\_online\/(?P<href>\d+)/', $movie->getElementsByTagName('a')->item(0)->getAttribute('href'), $tmpDataHref);
 
-                $responseInfo = $browser->get(sprintf('http://kinoland.pl/ajax.php?mode=movie&function=limit2&movieId=%s', $tmpDataHref['href']));
-                preg_match('/e=(?P<embed>.*?)\?/', $responseInfo, $tmpEmbed);
-
+                $responseInfo = $browser->get(sprintf('http://kinoland.pl/film_online/%s-dev.html', $tmpDataHref['href']));
+                preg_match('/megustavid.com\/e=(?P<embed>.*?)\?/', $responseInfo, $tmpEmbed);
+				
                 if ($em->getRepository('AcmeMainBundle:Movie')->findOneByRemoteKey($tmpDataHref['href'])) {
                     continue;
                 }
@@ -49,14 +57,14 @@ class MoviesCommand extends ContainerAwareCommand
 
                 $movieEntity = new Movie;
                 $movieEntity->setTitle($tmpData['title']);
-                $movieEntity->setText($movie->getElementsByTagName('div')->item(5)->nodeValue);
+                $movieEntity->setText($movie->getElementsByTagName('div')->item(2)->getElementsByTagName('div')->item(3)->nodeValue);
                 $movieEntity->setPhoto($movie->getElementsByTagName('img')->item(0)->getAttribute('src'));
                 $movieEntity->setTranslation($tmpData['translation']);
                 $movieEntity->setEmbed($tmpEmbed['embed']);
                 $movieEntity->setRemoteKey($tmpDataHref['href']);
                 $movieEntity->setRatingCount(0);
                 $movieEntity->setRatingValue(5);
-
+                
                 foreach (explode(',', str_replace(', ', ',', $tmpCategory['categories'])) as $catItem) {
                     $categoryName = trim($catItem);
 
